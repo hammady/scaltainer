32bd2af6583e5fbe14ba6701918d7c619ca2384f
Hossam Hammady <github@hammady.net>
2017-11-13 14:52:47 +0300

    Adding missing require 'json'

--- lib/scaltainer.rb
+++ lib/scaltainer.rb
@@ -4,4 +4,5 @@ require "scaltainer/service_types"
 require "scaltainer/runner"
 require "scaltainer/command"
 require "scaltainer/docker/service"
-require 'scaltainer/newrelic/metrics'
+require "scaltainer/newrelic/metrics"
+require "json"
--- lib/scaltainer/version.rb
+++ lib/scaltainer/version.rb
@@ -1,3 +1,3 @@
 module Scaltainer
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
